d389a37e38a3633530cdb577ba54a3c06de2045c
KOTHA-SRIVIBHU <kothasrivibhu.23bds1057@gmail.com>
2025-07-01 11:41:34 +0530

    render port added

--- app.py
+++ app.py
@@ -125,4 +125,5 @@ def index():
 
 if __name__ == '__main__':
     
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
